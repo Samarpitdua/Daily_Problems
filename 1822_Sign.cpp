029a0ea2c9c7892fcfe5cbfc21b04c317727811d
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-08-06 20:12:13 +0530

    Create 1822_Sign.cpp

---
+++ 1822_Sign.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int pos=0,zero=0,neg=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)
+                pos++;
+            else if(nums[i]<0)
+                neg++;
+            else
+                zero++;
+        }
+        if(zero)
+            return 0;
+        else if(neg%2==0)
+            return 1;
+        else
+            return -1;
+        
+        
+    }
+};
