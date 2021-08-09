4477bece5a308414b82b57706c50b0113b32a9b3
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-08-09 12:33:23 +0530

    Create 1672_Richest_Common_Wealth.cpp

---
+++ 1672_Richest_Common_Wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi=0;
+        for(auto x:accounts)
+        {
+            int ct=0;
+            for(auto y:x)
+            {
+                ct+=y;
+            }
+            if(ct>maxi)
+                maxi=ct;
+        }
+        return maxi;
+        
+    }
+};
