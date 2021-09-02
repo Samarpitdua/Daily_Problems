3b112c3098b1f34f5879ed89c99f25bfc48c456c
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-09-02 23:50:35 +0530

    Create 0094_Binary_tree_Inorder_traversal.cpp

---
+++ 0094_Binary_tree_Inorder_traversal.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     vector<int>v;
+    vector<int> inorderTraversal(TreeNode* root) {
+       
+        if(root==NULL)
+            return v;
+        inorderTraversal(root->left);
+        v.push_back(root->val);
+        inorderTraversal(root->right);
+        return v;
+    }
+};
+
+*********************************************
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode * root,vector<int>& v)
+    {
+        if(!root)return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>v;
+        if(!root)
+            return v;
+        inorder(root,v);
+        return v;
+    }
+};
