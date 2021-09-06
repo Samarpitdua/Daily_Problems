53c74ad8aa9ca4cfcc8ce5a78f0efa5184866445
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-09-06 10:24:47 +0530

    Create 1995_count_special_quadruples.cpp

---
+++ 1995_count_special_quadruples.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countQuadruplets(vector<int>& nums) {
+        int n=nums.size();
+        int ct=0;
+       // sort(nums.begin(),nums.end());
+        for(int i=0;i<n-3;i++)
+        {
+            for(int j=i+1;j<n-2;j++)
+            {
+                for(int k=j+1;k<n-1;k++)
+                {
+                    for(int l=k+1;l<n;l++)
+                    {
+                        if((nums[i]+nums[j]+nums[k])==nums[l])
+                        {
+                            ct++;
+                        }
+                    }
+                }
+            }
+        }
+        return ct;
+    }
+};
