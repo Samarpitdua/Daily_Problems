fd84f876619450068e2a229527aa97aedb4b2c51
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2021-09-09 01:29:04 +0530

    Create 0938_Range_sum_bst_tree.cpp

---
+++ 0938_Range_sum_bst_tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(TreeNode* root, int low, int high,int& sum)
+    {
+        if(!root)return;
+        if(root->val>high)
+            solve(root->left,low,high,sum);
+        else if(root->val<low)
+            solve(root->right,low,high,sum);
+        else{
+        sum+=root->val;
+        solve(root->left,low,high,sum);
+        solve(root->right,low,high,sum);
+        }
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum=0;
+        solve(root,low,high,sum);
+        return sum;
+    }
+};
