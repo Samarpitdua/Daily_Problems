0b0ae1d011d03238f66e8133dc490a5217d26cbc
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-01-07 20:35:26 +0530

    Time: 35 ms (10.32%), Space: 12.4 MB (16.26%) - LeetHub

---
+++ 506-relative-ranks/506-relative-ranks.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        vector<string> ans ;
+        map<int,int> mp;
+        vector<int> temp = score;
+        sort(temp.begin() , temp.end() , greater<int>()) ;
+        int ct = 1;
+        for(int i = 0 ;i < temp.size() ; i++)
+            mp[temp[i]] = ct++;
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(mp[score[i]] > 3)
+            {
+                ans.push_back(to_string(mp[score[i]]));
+            }
+            else if(mp[score[i]] == 1)
+                ans.push_back("Gold Medal");
+            else if(mp[score[i]] == 2)
+                ans.push_back("Silver Medal");
+            else if(mp[score[i]] == 3)
+                ans.push_back("Bronze Medal");
+            
+        }
+        return ans;
+        
+    }
+};
