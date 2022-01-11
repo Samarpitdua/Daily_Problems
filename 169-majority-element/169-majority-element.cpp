0397f0075a5b0e58e9291e1631dd2c41f180a041
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-01-11 14:26:27 +0530

    Time: 31 ms (18.66%), Space: 19.6 MB (87.80%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -18,41 +18,7 @@ public:
                     ct--;
             }
         }
-        
         return temp;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int ct=1;
-        // int temp=nums[0];
-        // for(int i=1;i<nums.size();i++)
-        // {
-        //     if(nums[i]==temp)
-        //         ct++;
-        //     else
-        //         ct--;
-        //     if(ct==0)
-        //         ct=1,temp=nums[i];
-        //     //cout<<temp<<" "<<ct<<"\n";
-        // }
-        // return temp;
     }
     
 };
