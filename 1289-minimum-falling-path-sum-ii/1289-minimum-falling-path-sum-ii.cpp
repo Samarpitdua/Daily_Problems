1bd3abadca4f649fec6eb9b78b756b80305e8637
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-02-11 23:47:24 +0530

    Time: 347 ms (45.73%), Space: 16.8 MB (10.66%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -6,32 +6,21 @@ public:
         if(n == 1)
             return grid[0][0];
         for(int i = 0 ; i < n ;i++)
-        {
             dp[n - 1][i] = grid[n - 1][i];
-        }
         for(int i = n - 2 ; i >= 0 ;i--)
         {
             for(int j = 0 ; j < n ; j++)
             {
                 long long  ans = INT_MAX;
-                
                 for(int k = 0 ; k < n ;k++)
-                {
-                    
                     if(k != j)
-                    {
                         ans = min(ans , dp[i + 1][k]);
-                    }
-                    
-                }
                 dp[i][j] = grid[i][j] + ans;
             }
         } 
         long long int ans = INT_MAX;
         for(int i = 0 ; i < n ;i++)
-        {
             ans = min(ans , dp[0][i]);
-        }
         return ans;
     }
 };
