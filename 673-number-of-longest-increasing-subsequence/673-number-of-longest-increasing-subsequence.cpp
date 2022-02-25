ea38b557e80f2d05d7e504321ad1dc64e2f3dcc6
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-02-26 01:38:00 +0530

    Time: 169 ms (62.29%), Space: 13.3 MB (51.07%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -3,8 +3,6 @@ public:
     int findNumberOfLIS(vector<int>& nums) {
         int ans = 0 , n = nums.size();
         vector<int> dp(n , 1) , dp2(n , 1);
-        
-        //dp[0] = 1;
         for(int i = 1 ; i < n ;i++)
         {
             int maxi = 0 , ct = 0;
@@ -21,16 +19,11 @@ public:
             dp2[i] = max(dp2[i] , ct);
         }
         int temp = *max_element(dp.begin() , dp.end());
-        // for(auto x : dp)
-        //     cout<<x<<" ";
-        // cout<<endl;
         for(int i = 0 ; i < n ; i++)
         {
             if(dp[i] == temp)
                 ans += dp2[i];
-                
         }
-        // cout<<endl;
         return ans;
         
     }
