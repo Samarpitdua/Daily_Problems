61db7db72b4d57a3714bbd561d11a4f3f8594734
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-03-21 19:27:09 +0530

    Time: 137 ms (48.45%), Space: 20 MB (86.86%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        set<vector<int>>st;
-        st.clear();
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>v;
-        int p=0;
-        for(int i=0;i<n-2;i++)
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin() , nums.end());
+        int start = 0;
+        
+        for(int i = 0 ; i < n ;i++)
         {
+            if(nums[i] > 0)
+                break;
+            if(i != 0 and nums[i - 1] == nums[i])
+                continue;
             
-            int cal=-nums[i];
-            for(int j=i+1,k=n-1;j<k;)
+            int left = i + 1 , right = n - 1;
+            while(left < right)
             {
-                if(nums[j]+nums[k]>cal)
-                    k--;
-                else if(nums[j]+nums[k]<cal)
-                    j++;
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum < 0)
+                    left++;
+                else if(sum > 0)
+                    right--;
                 else
                 {
-                    vector<int> triplet = {nums[i], nums[j], nums[k]};
-                    if(st.find(triplet)==st.end())
-                        v.push_back(triplet);
-                    st.insert(triplet);
-                    
-                    //p++;
-                    j++;
-                    k--;
+                    ans.push_back({nums[i] , nums[left] , nums[right]});  
+                    int val1 = nums[left] , val2 = nums[right];
+                    while(left < right and nums[left] == val1)
+                    {
+                        left++;
+                    }
+                    while(left , right and nums[right] == val2)
+                        right--;
                 }
+                
+                    
             }
         }
-        return v;
+        return ans;
     }
 };
