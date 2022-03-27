6c916b774ad8d1f4fa3d644a222184e2d2a439ea
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-03-28 01:12:44 +0530

    Time: 4 ms (76.93%), Space: 12.2 MB (11.28%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& p) {
-        int temp = p[0];
-        int n = p.size();
-        int a[n] , b[n] , c[n];
-        a[0] = -p[0] , b[0] = 0 , c[0] = 0;
-        for(int i = 1 ; i < p.size() ; i++)
+    int solve(vector<int>& p , int index , int isBuyPossible , vector<vector<int>>& dp)
+    {
+        if(index == p.size())
+            return 0;
+        if(dp[index][isBuyPossible] != -1)
+            return dp[index][isBuyPossible];
+        if(isBuyPossible == 2)
         {
-            a[i] = max(a[i-1] , -p[i] + c[i - 1]);
-            b[i] = max(b[i-1] , a[i - 1] + p[i]);
-            c[i] = max(c[i-1] , b[i - 1]);
+            return dp[index][isBuyPossible] = max(-p[index] + solve(p , index + 1 , 0 , dp) , solve(p , index + 1 , 2 , dp));
         }
-        return b[n - 1];
+        else if(isBuyPossible == 0)
+             return dp[index][isBuyPossible] = max(p[index] + solve(p, index + 1 , 1 , dp) ,solve(p ,index + 1 , 0 ,dp));
+        else
+            return dp[index][isBuyPossible] = solve(p , index + 1 , 2 , dp);
+            
+    }
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n + 1, vector<int>(3 , -1));
+        int ans = solve(p , 0 , 2 , dp);
+        return ans;
+        
     }
 };
