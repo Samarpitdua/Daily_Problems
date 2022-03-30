bed9b18ec867dedf3b9e1b042555d35ba78de79e
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-03-30 11:11:33 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (25.47%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -14,7 +14,15 @@ public:
         return dp[start][end] = solve(start + 1 , end , m  , n , dp) + solve(start , end + 1 , m , n , dp);
     }
     int uniquePaths(int m , int n) {
-        vector<vector<int>> dp(m + 1 , vector<int>(n + 1 , -1));
-        return solve(0 , 0 , m , n , dp);
+        vector<vector<int>> dp(m + 1 , vector<int>(n + 1 , 1));
+        
+        for(int i = m - 2 ; i >= 0 ; i--)
+        {
+            for(int j = n - 2 ; j >= 0 ; j--)
+            {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+        return dp[0][0];
     }
 };
