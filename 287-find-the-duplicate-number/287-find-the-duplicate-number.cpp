2d2e7c547f92245776e3706132c7796359873a99
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-03-30 11:37:55 +0530

    Time: 155 ms (57.78%), Space: 61.3 MB (26.64%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     //We will use hare and tortoise approach which is very common in linked list 
     int findDuplicate(vector<int>& nums) {
-        int i = 0;
-        int fast = nums[0] , slow = nums[0];
+        int i = 0 , slow = 0 , fast = 0;
         while(1)
         {
-            fast = nums[nums[fast]];
             slow = nums[slow];
-            if(fast == slow)
+            fast = nums[nums[fast]];
+            if(slow == fast)
                 break;
         }
-        fast = nums[0];
+        fast = 0;
         while(fast != slow)
         {
-            slow = nums[slow];
             fast = nums[fast];
+            slow = nums[slow];
         }
         return slow;
     }
