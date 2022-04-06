24f02bc5a8672341ebcfeabfa58176ff45d3c777
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-04-06 16:39:05 +0530

    Time: 7 ms (44.62%), Space: 11.1 MB (29.69%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -6,24 +6,23 @@ public:
         int high = n - 1;
         while(low <= high)
         {
-            int mid = (high + low) / 2;
+            int mid = low + (high - low) / 2;
             if(nums[mid] == target)
                 return mid;
-            if(nums[mid] >= nums[low] )
+            else if(nums[low] <= nums[mid])
             {
-                if(target >= nums[low] and target <= nums[mid])
+                if(target >= nums[low] and target < nums[mid])
                     high = mid - 1;
                 else
                     low = mid + 1;
             }
             else
             {
-            if(target <= nums[high] and target >= nums[mid])
-                low = mid + 1;
-            else
-                high = mid - 1;
+                if(target >= nums[mid] and target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
-            
         }
         return -1;
     }
