d6feff9eb3c6901067baa70dcf9f6ff9e16f88c1
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-04-06 15:52:21 +0530

    Added solution - LeetHub

---
+++ Find Nth root of M - GFG/find-nth-root-of-m.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	long long multiply(int a,int b,int m)
+	{
+	    long long ans = 1;
+	    for(int i = 0 ; i < b ; i++)
+	    {
+	        ans *= a;
+	        if(ans > m)
+	            return -1;
+	    }
+	    if(ans == m)
+	        return -2;
+	    return ans;
+	}
+	//2,4,8,16...
+	//3,9,27,81...
+	//As it is monotnoically increasing, so we can apply binary search
+	//The naswe will always lie between 1 and m
+	//shrink the array till the diffrence between high and low is<1e-6 or 5 decimal spaces error
+	int NthRoot(int n, int m)
+	{
+	    int low = 1 , high = m;
+	    if(m == 1)
+	        return 1;
+	   int ans;
+	    while(low <= high)
+	    {
+	        int mid = low + (high - low)/2;
+	        int temp = multiply(mid , n , m);
+	        if(temp == -2)
+	            return mid;
+	        else if(temp == -1)
+	        {
+	            high = mid - 1;
+	        }
+	        else
+	           ans = low ,  low = mid + 1;
+	    }
+	    low = ans;
+	    ans = 1;
+	    //cout<<low<<" ";
+	    for(int i = 0 ; i < n ;i++)
+	    {
+	        ans *= low;
+	    }
+	    if(ans == m )
+	        return low;
+	    return -1;
+	    // Code here.
+	}  
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}  // } Driver Code Ends
