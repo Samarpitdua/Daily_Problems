3b61e4bf3d021d02a4340698e53765b6969b718f
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-04-09 16:33:37 +0530

    Time: 8 ms (26.53%), Space: 10.6 MB (5.03%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     //O(1) space
-    void solve2(vector<vector<int>>& ans , vector<int>& nums , int ind)
+    void solve2(vector<vector<int>>& ans , vector<int> nums , int ind)
     {
         if(ind == nums.size())
         {
@@ -12,27 +12,28 @@ public:
         {
             swap(nums[i] , nums[ind]);
             solve2(ans , nums ,ind + 1);
-            swap(nums[i] , nums[ind]);
+            //swap(nums[i] , nums[ind]);
         } 
     }
     
     //O(n) space
-    void solve(vector<vector<int>>& ans , vector<int>& v , vector<int>& nums ,map<int ,int>& mp)
+    void solve(vector<vector<int>>& ans , vector<int>& v , vector<int>& nums , map<int , int> mp)
     {
         if(v.size() == nums.size())
         {
+            //cout<<"hi";
             ans.push_back(v);
-            return;
+            return ;
         }
         for(auto x : nums)
         {
-            if(!mp[x])
+            if(mp.find(x) == mp.end())
             {
                 mp[x] = 1;
                 v.push_back(x);
-                solve(ans , v , nums ,mp);
-                mp[x] -= 1;
+                solve(ans , v , nums , mp);
                 v.pop_back();
+                mp.erase(x);
             }
         }
     }
@@ -40,8 +41,8 @@ public:
         vector<vector<int>> ans;
         map<int , int> mp;
         vector<int> v;
-        //solve(ans , v ,  nums , mp);
-       solve2(ans , nums ,0);
+        solve(ans , v ,  nums , mp);
+       //solve2(ans , nums ,0);
         return ans;
         
     }
