73111c06628776dd8593c0a1b1cbb07137b06a54
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-04-10 13:52:20 +0530

    Time: 345 ms (27.25%), Space: 65.7 MB (74.58%) - LeetHub

---
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        int ct = 0;
+        for(int i = 1; i < nums.size() ;i++)
+        {
+            if(nums[i] <= nums[i - 1])
+            {
+                ct += (abs(nums[i] - nums[i - 1] ) + 1);
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ct;
+    }
+};
