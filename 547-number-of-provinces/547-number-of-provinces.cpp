cb2fef6b23e835a20b006862ab7a8cf3f2082326
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-04-20 22:08:38 +0530

    Time: 19 ms (95.96%), Space: 14 MB (37.98%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     void makeSet(vector<int>& parent)
     {
-        for(int i=0;i<205;i++)
-            parent[i]=i;
+        for(int i = 0 ; i < 205 ; i++)
+            parent[i] = i;
     }
     int findParent(int node,vector<int>& parent)
     {
-        if(parent[node]==node)
+        if(parent[node] == node)
             return node;
         else
-            return parent[node]=findParent(parent[node],parent);
+            return parent[node] = findParent(parent[node] , parent);
     }
-    void combine(int u,int v,vector<int>& parent,vector<int>& rank)
+    void combine(int u , int v , vector<int>& parent , vector<int>& rank)
     {
-        u=findParent(u,parent);
-        v=findParent(v,parent);
+        u = findParent(u,parent);
+        v = findParent(v,parent);
          if(rank[v]>rank[u])
-            parent[u]=parent[v];   
+            parent[u] = parent[v];   
         else if(rank[u]>rank[v])
             parent[v]=parent[u];
          else
-             parent[u]=v,rank[v]++;
+             parent[u] = v , rank[v]++;
     }
     int findCircleNum(vector<vector<int>>& a) {
-        vector<int> rank(205 , 0);
+        int n = a.size();
+        vector<int> rank(205, 0);
         vector<int> parent(205);
         makeSet(parent);
-        int n = a.size();
+        
         for(int i = 0 ; i < n ; i++)
         {
             for(int j = i + 1 ; j < n ; j++)
@@ -36,10 +37,10 @@ public:
                     combine(i , j , parent , rank);
             }
         }
-        int ct=0;
-        for(int i=0;i<a.size();i++)
+        int ct = 0;
+        for(int i = 0 ; i < n ; i++)
         {
-            if(i==parent[i])
+            if(i == parent[i])
                 ct++;
         }
         return ct;
