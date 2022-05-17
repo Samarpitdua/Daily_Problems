4829f94c541f67a7ee29b0e09c633f1ccec4bfc9
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-17 10:44:55 +0530

    Time: 14 ms (15.07%), Space: 16.4 MB (18.27%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<int> temp , int index , int n , vector<vector<int>>& ans)
+    void solve(vector<int>& nums , vector<int> temp , vector<vector<int>>& ans , int n , int ind)
     {
+        if(ind == n)
+        {
             ans.push_back(temp);
-            while(index < n)
-            {
-                temp.push_back(nums[index]);
-                solve(nums , temp , index + 1 , n , ans);
-                temp.pop_back();
-                while(index < (n - 1) and nums[index] == nums[index + 1])
-                    index++;
-                index++;
-            }
+            return;
+        }
+            temp.push_back(nums[ind]);
+            solve(nums , temp , ans , n , ind + 1);
+            temp.pop_back();
+            while(ind < n - 1 and nums[ind] == nums[ind + 1])
+                ind++;
+             solve(nums , temp , ans , n , ind + 1);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin() , nums.end());
         vector<int> temp;
+        sort(nums.begin() , nums.end());
         int n = nums.size();
-        solve(nums , temp , 0 , n , ans);
+        solve(nums , temp , ans , n , 0);
         return ans;
-        
     }
 };
