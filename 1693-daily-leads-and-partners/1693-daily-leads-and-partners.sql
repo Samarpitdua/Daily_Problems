af6be97190fa965cf6de23bcc1c3637c432ef1f8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-19 16:04:29 +0530

    Time: 486 ms (75.33%), Space: 0B (100.00%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select date_id , make_name , count(distinct(lead_id)) as unique_leads , count(distinct(partner_id))as unique_partners from dailySales group by 1, 2;
+select date_id , make_name ,
+count(distinct(lead_id)) as unique_leads ,
+count(distinct(partner_id))as unique_partners
+from dailySales
+group by 1, 2;
