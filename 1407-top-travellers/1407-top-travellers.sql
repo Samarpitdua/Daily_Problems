f96b09746bdec359bd97b20041813b9699f41d07
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-21 16:03:10 +0530

    Time: 1095 ms (13.68%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,8 +1,30 @@
-select 
-	name,
-	sum(if(distance is not null, distance, 0)) as travelled_distance
-from users u 
-left join rides r 
-	on u.id = r.user_id 
-group by name
-order by 2 desc, 1 ;
+
+select name , if(sum(distance) is null , 0 , sum(distance)) as travelled_distance from users left join rides on users.id = rides.user_id group by users.id order by 2 desc, 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# select 
+# 	name,
+# 	sum(if(distance is not null, distance, 0)) as travelled_distance
+# from users u 
+# left join rides r 
+# 	on u.id = r.user_id 
+# group by name
+# order by 2 desc, 1 ;
