00d03f3bec46db40208dab0e298423f41fc4b3d7
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-21 09:53:15 +0530

    Time: 707 ms (9.30%), Space: 6.5 MB (87.29%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,60 +1,34 @@
-// class Solution {
-// public:
-//     bool isPallin(string s , int i , int j)
-//     {
-//         while(i < j)
-//         {
-//             if(s[i] != s[j])return false;
-//             i++ , j--;
-//         }
-//         return true;
-//     }
-//     int minCut(string s) {
-//         int n = s.size();
-//         vector<int> dp(n + 1 , 0);
-//         for(int i = n - 1 ; i >= 0 ;i--)
-//         {
-//             int ans= INT_MAX;
-//             for(int j = i ; j < n ;j++)
-//              {
-//                 int a = 0;
-//                     a = isPallin(s , i , j);
-//                 if(a)
-//                 {
-//                      ans = min(ans , 1 + dp[j + 1]);
-//                 }
-//             }
-//             dp[i] = ans;
-//         }
-//         return dp[0] - 1;
-//     }
-// };
 class Solution {
 public:
-
-bool isPalindrome(string &s, int start, int end){
-    while(start < end){
-        if(s[start] != s[end]) return false;
-        start++, end--;
+    bool isPallin(string& s , int i , int j)
+    {
+        while(i < j)
+        {
+            if(s[i] != s[j])
+                return false;
+            i++ , j--;
+        }
+        return true;
     }
-    return true;
-}
-
-int f(int i, string &s, vector<int> &dp){
-    
-    if(i >= s.length()) return 0;
-    if(dp[i] != -1) return dp[i];
-    
-    int mini = INT_MAX;
-    for(int j=i;j<s.length();j++){
-        if(isPalindrome(s, i, j)) mini = min(mini, 1 + f(j+1, s, dp));
+    int solve(string& s , int i , vector<int>& dp)
+    {
+        if(i >= s.size())
+            return 0;
+        if(dp[i] != -1)
+            return dp[i];
+        
+        int ans = INT_MAX;
+        for(int j = i ; j < s.size() ;j++)
+        {
+            if(isPallin(s , i , j))
+            {
+                 ans = min(ans , 1 + solve(s , j + 1 , dp));
+            }
+        }
+        return dp[i] = ans;
     }
-    return dp[i] = mini;
-}
-
-int minCut(string s){
-    
-    vector<int> dp(s.length(), -1);
-    return f(0, s, dp) - 1;
-}
+    int minCut(string s) {
+        vector<int> dp(s.size() , -1);
+        return solve(s , 0, dp) - 1;
+    }
 };
