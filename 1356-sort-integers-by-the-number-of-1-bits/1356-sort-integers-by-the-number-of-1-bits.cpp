fe73c9db82ab953a13ff89cf369f8fd0650dbd7e
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-25 01:05:08 +0530

    Time: 6 ms (91.98%), Space: 10.2 MB (72.11%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+
+    bool comp(int a , int b)
+    {
+        if( __builtin_popcount(a) == __builtin_popcount(b))
+            return a < b;
+        else 
+            return ( __builtin_popcount(a) < __builtin_popcount(b));
+    }
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin() , arr.end() , comp);
+        return arr;
+    }
+};
