89d9975f9aa10e2c7ac67c08706d1e1c6b5d33c2
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-05-30 00:21:49 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.55%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        long long a = 1, b = 1 , c;
+        if(n <= 1)
+            return 1;
+        
+        for(int i = 2 ; i <= n;i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+        
+    }
+};
