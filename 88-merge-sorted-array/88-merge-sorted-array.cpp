23bdc00c57d78517ae8f6231247325bf91452b68
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-06-07 09:37:25 +0530

    Time: 4 ms (49.07%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int gap = ceil((m + n) / (double)2);
-        cout<<gap<<" ";
         for(int i = m , j = 0; i < nums1.size() ;i++)
         {
             nums1[i] = nums2[j++];
