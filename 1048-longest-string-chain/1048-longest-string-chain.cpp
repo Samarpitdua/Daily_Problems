93dcdcd15ff297062e86dc92fbcdf00988595e2f
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-06-15 21:57:47 +0530

    Time: 68 ms (91.79%), Space: 17.3 MB (51.94%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestStrChain(vector<string>& words) {
+        unordered_map<string, int> mp;
+        int ans = 1;
+        
+        sort(words.begin(), words.end(), [](const string &l, const string &r)
+             { return l.size() < r.size(); }
+        );
+        for (string word : words) 
+        {
+            mp[word] = 1;
+            for (int i = 0; i < word.size(); i++) 
+            {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                if (mp.find(prev) != mp.end()) 
+                {
+                    mp[word] = mp[prev] + 1;
+                    ans = max(ans, mp[word]);
+                }
+            }
+        }
+        return ans;
+    }
+};
