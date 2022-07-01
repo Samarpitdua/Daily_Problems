0c6038131faf66fb4ab32d3239aa6d9a2b928e76
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-01 11:40:39 +0530

    Time: 2 ms (64.45%), Space: 5.9 MB (41.38%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+       int a = 0 , b = 1 , c;
+        if(!n)
+            return a;
+        if(n == 1)
+            return b;
+        for(int i = 2 ;i <= n ;i++)
+            c = a + b , a = b , b = c;
+        return c;
+    }
+};
