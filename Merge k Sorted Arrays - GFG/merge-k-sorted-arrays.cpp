7cf6173b22fe773e5f5c6bad3d278c9b76bb25af
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-03 16:53:39 +0530

    Added solution - LeetHub

--- Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
+++ Merge k Sorted Arrays - GFG/merge-k-sorted-arrays.cpp
@@ -14,20 +14,35 @@ for (int i=0; i < size; i++)
  // } Driver Code Ends
 //User function Template for C++
 
-
+#define pii pair<int , pair<int , int>>
 class Solution
 {
     public:
     //Function to merge k sorted arrays.
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    vector<int> mergeKArrays(vector<vector<int>> arr, int k)
     {
         vector<int> ans;
-        for(auto x  : arr)
-        {
-            for(auto y : x)
-            ans.push_back(y);
+        priority_queue<pii , vector<pii> , greater<pii>> pq;
+        for(int i = 0 ; i < k ;i++){
+            pq.push({arr[i][0] , {i , 0}});
         }
-        sort(ans.begin() , ans.end());
+        while(1)
+        {
+            if(ans.size() == k * k)
+                break;
+            auto temp = pq.top();
+            pq.pop();
+            int f = temp.first;
+            int sf = temp.second.first;
+            int ss = temp.second.second;
+            ans.push_back(f);
+            if(ss + 1 < k)
+            {
+                pq.push({arr[sf][ss + 1] , {sf , ss + 1}});
+            }
+            
+        }
+        return ans;
         return ans;
         //code here
     }
