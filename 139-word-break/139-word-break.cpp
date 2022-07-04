18453d44a460f8613b57b69b85750eb208ba8ae8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-04 18:58:25 +0530

    Time: 39 ms (22.89%), Space: 14.6 MB (23.42%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    map<string , bool> mp;
+    
+    bool wordBreak(string s, vector<string>& words)
+    {
+        if(find(words.begin() , words.end() , s) != words.end())
+            return true;
+        if(mp.find(s) != mp.end())
+            return mp[s];
+        for(int i = 1 ; i < s.size() ; i++)
+        {
+            string curr = s.substr(0 , i);
+            if(find(words.begin() , words.end() , curr) != words.end())
+            {
+                
+                if(wordBreak(s.substr(i , s.size()) , words))
+                {
+                    mp[s] = true;
+                    return true;
+                }
+            }
+          
+        }
+        mp[s] = false;
+        return false;
+        
+    }
+};
