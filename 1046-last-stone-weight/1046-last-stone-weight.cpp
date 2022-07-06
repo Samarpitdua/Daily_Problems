cd43c34f60b34eb0cff31009df3dc54a8edee5ec
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-06 11:27:37 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (33.40%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -4,17 +4,16 @@ public:
         priority_queue<int> pq;
         for(auto x : stones)
             pq.push(x);
-        
-        while( pq.size() > 1) 
-        {
-            int a = pq.top();
+        while(pq.size() > 1){
+            int temp = pq.top();
             pq.pop();
-            int b = pq.top();
+            int temp2 = pq.top();
             pq.pop();
-            pq.push(a - b);
-            if(pq.top() == 0)
-                return 0;
+            if(temp != temp2)
+                pq.push(temp - temp2);
         }
+        if(pq.empty())
+            return 0;
         return pq.top();
     }
 };
