e38766dddaad8af445a22b7179fb984bf107972b
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-09 14:24:03 +0530

    Time: 479 ms (29.53%), Space: 18 MB (11.01%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -9,7 +9,7 @@ public:
         if(dp[i] != -1)
             return dp[i];
         int ans = 10000;
-        for(int j = 1 ; j <= nums[i] ;j++){
+        for(int j = 1 ; j <= nums[i] and (i + j) < n ;j++){
             ans = min(ans , 1 + solve(nums , i + j , dp , n));
         }
         return dp[i] = ans;
