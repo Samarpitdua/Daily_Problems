33511d989dd201bde2c9ef9e38105992865dd6f2
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-15 02:28:20 +0530

    Time: 27 ms (31.60%), Space: 10.3 MB (24.53%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
+    int solve(int n , int i , int j , vector<vector<int>>& mat , vector<vector<int>>& dp){
+        
+        if(j >= n or j < 0)
+            return 1e8;
+        if(i == n - 1){
+            return mat[i][j];
+        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        // return 0;
+        return dp[i][j] = mat[i][j] + min({solve(n , i + 1 , j, mat , dp) , solve(n , i + 1 , j - 1, mat , dp) , solve(n , i + 1 , j + 1, mat , dp)});
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp(n , vector<int>(n , 0));
+        vector<vector<int>> dp(n + 1 , vector<int>(n + 1 , -1));
         if(n == 1)
             return matrix[0][0];
-        
-        for(int i = 0 ; i < n ;i++)
-        {
-            dp[n - 1][i] = matrix[n -1][i];
-        }
-        for(int i = n - 2 ; i >= 0 ; i--)
-        {
-            for(int j = 0 ; j < n ; j++)
-            {
-                if(j == 0)
-                {
-                    dp[i][j] = matrix[i][j] + min(dp[i + 1][j] , dp[i + 1][j + 1]);
-                }
-                else if(j == n - 1)
-                {
-                    dp[i][j] = matrix[i][j] + min(dp[i + 1][j] , dp[i + 1][j - 1]);
-                }
-                else
-                    dp[i][j] = matrix[i][j] + min({dp[i + 1][j] , dp[i + 1][j + 1] , dp[i + 1][j - 1]});
-            }
-        }
-        int ans = INT_MAX;
-        for(int i = 0 ; i < n ; i++)
-        {
-            ans = min(ans , dp[0][i]);
+        int ans = 10000;
+        for(int i = 0 ;i < n;i++){
+            ans = min(ans , solve(n , 0 , i , matrix , dp));
         }
         return ans;
     }
