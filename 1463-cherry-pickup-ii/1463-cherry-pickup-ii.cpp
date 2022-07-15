3fb698f1e49fb2136aa74d18880083d82d7fadeb
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-15 22:39:59 +0530

    Time: 191 ms (31.49%), Space: 17 MB (9.17%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int solve(int i , int j1 , int j2 , vector<vector<int>>& grid , vector<vector<vector<int>>>& dp, int m , int n)
+int solve(int i , int j1 , int j2 , vector<vector<int>>& grid , vector<vector<vector<int>>>& dp , int n)
     {
         if(j1 >= n or j1 < 0 or j2 >= n or j2 < 0)
             return -1e8;
-        if(i == m - 1){
+        
+        if(i == grid.size() - 1)
+        {
             if(j1 == j2)
                 return grid[i][j1];
             else
                 return grid[i][j1] + grid[i][j2];
         }
+        
         if(dp[i][j1][j2] != -1)
             return dp[i][j1][j2];
+        
         int maxi = -1e8;
         for(int j11 = -1 ; j11 <= 1 ;j11++){
             for(int j22 = -1 ;j22 <= 1 ;j22++){
@@ -20,7 +24,7 @@ public:
                     val += grid[i][j1];
                 else
                     val += grid[i][j1] + grid[i][j2];
-                maxi = max(maxi , val + solve(i + 1 , j1 + j11 , j2 + j22, grid , dp , m, n));
+                maxi = max(maxi , val + solve(i + 1 , j1 + j11 , j2 + j22, grid , dp, n));
                     
             }
         }
@@ -30,7 +34,7 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
         vector<vector<vector<int>>> dp(rows , vector<vector<int>>(cols , vector<int>(cols , -1)));
-        return solve(0 , 0 , cols - 1 , grid , dp , rows , cols);
+        return solve(0 , 0 , cols - 1 , grid , dp , cols);
         
     }
 };
