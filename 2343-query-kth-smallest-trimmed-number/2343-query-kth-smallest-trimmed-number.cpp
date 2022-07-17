b5eb71f2761f15722151352beb2b8b68957d65f4
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-17 13:15:48 +0530

    Time: 2268 ms (11.11%), Space: 369.8 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        vector<int> res;
+        vector<int> ans;
         int n = nums.size();
-        for(auto &q : queries) {
+        for(auto q : queries) {
             set<pair<string,int>> st;
-            for(int i=0;i<n; i++) {
-                st.insert({(nums[i].substr(nums[i].size()-q[1])), i});
-                // pq.push_back({(nums[i].substr(nums[i].size()-q[1])), i});
+            for(int i = 0 ; i < n ; i++) 
+            {
+                st.insert({(nums[i].substr(nums[i].size() - q[1])) ,  i});
             }
             auto x = *std::next(st.begin(), q[0] - 1);
-            res.push_back(x.second);
+            ans.push_back(x.second);
             }
-        return res;
+        return ans;
     }
 };
