f0a0d8ac02ba121d8a0d15f813c80c393f010e74
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-17 14:23:43 +0530

    Time: 532 ms (26.93%), Space: 10.6 MB (24.23%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    //Using Binary Search
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp;
-        for(auto x : nums)
-        {
-            auto it = lower_bound(dp.begin() , dp.end() , x);
-            if(it == dp.end())
-            {
-                dp.push_back(x);
-            }
-            else{
-                *it = x;
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n + 1 , 1);
+        for(int i = 1 ; i < n ;i++){
+            for(int j = 0 ; j < i ;j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = max(dp[i] , 1 + dp[j]);
+                }
             }
         }
-        return dp.size();
+        return *max_element(dp.begin() , dp.end());
     }
 };
