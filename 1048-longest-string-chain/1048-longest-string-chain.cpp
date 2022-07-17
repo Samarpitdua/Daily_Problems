7c961bf82b8c88a5ff1494a97880892805d032bc
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-18 01:53:59 +0530

    Time: 125 ms (62.12%), Space: 13.1 MB (90.25%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    int longestStrChain(vector<string>& words) {
-        unordered_map<string, int> mp;
-        int ans = 1;
-        
-        sort(words.begin(), words.end(), [](const string &l, const string &r)
-             { return l.size() < r.size(); }
-        );
-        for (string word : words) 
-        {
-            mp[word] = 1;
-            for (int i = 0; i < word.size(); i++) 
+    static bool comp(string& s , string& t){
+        return s.size() < t.size();
+    }
+    bool find(string& s , string& t){
+        int n = s.size() , m = t.size();
+        if(n != (m + 1))
+            return false;
+        int ct = 0;
+        for(int i = 0 , j = 0; i < n and j < n;){
+            if(s[i] == t[j])
             {
-                string prev = word.substr(0, i) + word.substr(i + 1);
-                if (mp.find(prev) != mp.end()) 
-                {
-                    mp[word] = mp[prev] + 1;
-                    ans = max(ans, mp[word]);
-                }
+                i++ , j++;
+                continue;
+            }
+            else
+            {
+                if(ct)
+                    return false;
+                else
+                    ct = 1;
+             i++;           
             }
         }
-        return ans;
+        return true;
+    }
+    void solve(vector<string>& nums , int n , vector<int>& dp){
+	    for(int i = 1; i < n ;i++){
+	        for(int j =0 ; j < i ;j++){
+	            if(find(nums[i] , nums[j]))
+	                dp[i] = max(dp[i] , 1 + dp[j]);
+	        }
+            // cout<<dp[i]<<" ";
+	    }
+        
+	}
+
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        sort(words.begin() , words.end() ,comp);
+        vector<int> dp(n , 1);
+        solve(words , n , dp);
+        return *max_element(dp.begin() , dp.end());
     }
 };
