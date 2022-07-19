1ce9ee445224d343de4b95ea465436daa18082e5
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-19 11:02:21 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
-    //As we proceed at each row we get equal number of elements
-    //If you want to find a value at a particualar row and column , the formula is-> r-1^C^c-1
-    //---->O(n)complexity
-    //Let's suppose you want to find any full row then observe the pattern
-    //res*=(n-i),res/=(i+1);
-    vector<vector<int>> generate(int n) {
-        vector<vector<int>> ans(n);
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
         vector<int> temp;
-        for(int i = 0 ; i < n ;i++)
-        {
-            temp.resize(i + 1);
-            if(i == 0)
-            {
-                temp[0] = 1;
-                ans[i] = temp;
-                continue;
+        for(int i = 1 ;i <= numRows ; i++){
+            temp.resize(i);
+            temp[0] = 1;
+            temp[i - 1] = 1;
+            for(int j = 1 ;j < i - 1 ;j++){
+                temp[j] = ans[i - 2][j - 1] + ans[i - 2][j];
             }
-            temp[0] = 1 , temp[i] = 1;
-            for(int j = 1 ; j < i ; j++)
-            {
-                temp[j] = ans[i - 1][j - 1] + ans[i - 1][j];
-            }
-            ans[i] = temp;
+            ans.push_back(temp);
         }
         return ans;
-        
-        
-    // vector<vector<int>>ans(numRows);
-    //     for(int i=0;i<numRows;i++)
-    //     {
-    //         ans[i].resize(i+1);
-    //         ans[i][0]=ans[i][i]=1;//First and Last element will always be 1.
-    //         for(int j=1;j<i;j++)
-    //         {
-    //             ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
-    //         }
-    //     }
-    //     return ans;
     }
 };
