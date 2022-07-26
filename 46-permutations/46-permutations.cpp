9a4078adb6b31cd92e266a8f9ac1ae4099a535d9
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-26 19:44:54 +0530

    Time: 6 ms (46.00%), Space: 7.8 MB (54.60%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,47 +1,21 @@
 class Solution {
 public:
-    //O(1) space
-    void solve2(vector<vector<int>>& ans , vector<int> nums , int ind)
-    {
+    void solve(vector<int>& nums , int ind  , vector<vector<int>>& ans){
         if(ind == nums.size())
         {
             ans.push_back(nums);
             return;
         }
-        for(int i = ind ; i < nums.size() ;i++)
-        {
-            swap(nums[i] , nums[ind]);
-            solve2(ans , nums , ind + 1);
-        } 
-    }
-    
-    //O(n) space
-    void solve(vector<vector<int>>& ans , vector<int> v , vector<int>& nums , map<int , int> mp)
-    {
-        if(v.size() == nums.size())
-        {
-            ans.push_back(v);
-            return ;
+        for(int i = ind; i < nums.size() ;i++){
+            swap(nums[ind] , nums[i]);
+            solve(nums , ind + 1 , ans);
+            swap(nums[ind] , nums[i]);
         }
-        for(auto x : nums)
-        {
-            if(mp.find(x) == mp.end())
-            {
-                mp[x] = 1;
-                v.push_back(x);
-                solve(ans , v , nums , mp);
-                v.pop_back();
-                mp.erase(x);
-            }
-        }
+        
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        map<int , int> mp;
-        vector<int> v;
-       // solve(ans , v ,  nums , mp);
-       solve2(ans , nums ,0);
+        solve(nums , 0 , ans);
         return ans;
-        
     }
 };
