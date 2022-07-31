741c5cd1625a1ce46b5297aac20a1eac1cff29f3
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-07-31 22:02:55 +0530

    Time: 284 ms (83.89%), Space: 108.3 MB (84.34%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int shortestSequence(vector<int>& rolls, int k) {
-        map<int , int> mp;
+        unordered_map<int , int> mp;
         int ct = 0 , ans = 0;
         for(auto x : rolls){
             mp[x]++;
