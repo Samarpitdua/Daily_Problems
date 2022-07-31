e1de5e0cdec531b5d5174ed83232bab507dd48cf
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-08-01 01:12:58 +0530

    Time: 1115 ms (23.05%), Space: 166.3 MB (31.12%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -0,0 +1,34 @@
+class FoodRatings {
+public:
+    map<string , set<pair<int , string>>> mp;
+    map<string , string> fo;
+    map<string , int> rat;
+    FoodRatings(vector<string>& foods, vector<string>& c, vector<int>& ratings) {
+        for(int i = 0 ; i < foods.size() ;i++){
+            
+            mp[c[i]].insert({-ratings[i] , foods[i]});
+            fo[foods[i]] = c[i];
+            rat[foods[i]] = -ratings[i];
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        string cuisine = fo[food];
+        int rating = rat[food];
+        mp[cuisine].erase({rating , food});
+        mp[cuisine].insert({-newRating , food});
+        rat[food] = -newRating;
+    }
+    
+    string highestRated(string cuisine) {
+        auto p = *(mp[cuisine].begin());
+        return p.second;
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
