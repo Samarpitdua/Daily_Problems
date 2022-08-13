082922f3c54c55f0d624ec0fb45f0a5f263605e8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-08-13 23:02:58 +0530

    Time: 67 ms (27.33%), Space: 18.7 MB (76.00%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.cpp
+++ 986-interval-list-intersections/986-interval-list-intersections.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
-vector<vector<int>> intervalIntersection(vector<vector<int>>& v, vector<vector<int>>& v2) {
-    vector<vector<int>> ans;
-    int n = v.size() , m = v2.size();
-    for(int i = 0 , j = 0 ; i < n and j < m ;)
-    {
-        //cout<<i<<" "<<j<<endl;
-        int x = v[i][0] , y = v[i][1];
-        int p = v2[j][0] , q = v2[j][1];
-        vector<int> temp;
-        
-         if(p < x and q < x)
-        {
-            j++;
-            continue;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& fl, vector<vector<int>>& sl) {
+        vector<vector<int>> ans;
+        int f = fl.size() , s = sl.size();
+        for(int i = 0 , j = 0 ; i < f and j < s ;){
+            if(fl[i][1] < sl[j][0])
+                i++;
+            else if(sl[j][1] < fl[i][0])
+                j++;
+            else{
+                int st = max(fl[i][0] , sl[j][0]);
+                int en = min(fl[i][1] , sl[j][1]);
+                ans.push_back({st , en});
+                if(fl[i][1] < sl[j][1])
+                    i++;
+                else
+                    j++;
+            }
         }
-         if(p > x and p > y)
-        {
-            i++;
-            continue;
-        }
-        if(x <= p and y >= q)
-        {
-            temp.push_back(p);
-            temp.push_back(q);
-            j++;
-        }
-        else if(p <= x and q >= y)
-        {
-            temp.push_back(x);
-            temp.push_back(y);
-            i++;
-        }
-        else if(x <= p and y <= q)
-        {
-            temp.push_back(p);
-            temp.push_back(y);
-            i++;
-        }
-        else if(x >= p and y >= q)
-        {
-            temp.push_back(x);
-            temp.push_back(q);
-            j++;
-        }
-        //else if()
-        ans.push_back(temp);
-    }
         return ans;
-}
+        
+    }
 };
