e431bcd00d8dfe96cd61dc285ebd547c5983b8ae
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-09-10 19:00:00 +0530

    Time: 103 ms (25.08%), Space: 8.7 MB (92.47%) - LeetHub

---
+++ 368-largest-divisible-subsetdiv-classcss-1x6t3yla-titleedit-this-question-href-library-368-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-368-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div/368-largest-divisible-subsetdiv-classcss-1x6t3yla-titleedit-this-question-href-library-368-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-368-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int solve(vector<int>& nums , int n , vector<int>& dp , vector<int>& ans){
+        int f = 0 , maxi = 0;
+	    for(int i = 1; i < n ;i++){
+	        for(int j =0 ; j < i ;j++){
+	            if(nums[i] % nums[j] == 0)
+                {
+                    if(dp[i] <= dp[j])
+                    {
+                        dp[i] = 1 + dp[j];
+                        ans[i] = j;
+                    }
+                }
+	        }
+            if(dp[i] > maxi)
+            {
+                maxi = dp[i];
+                f = i;
+            }
+	    }
+        return f;
+	}
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        vector<int> dp(n , 1) , ans(n , -1) , temp;
+        int f = solve(nums ,n , dp , ans);
+        temp.push_back(nums[f]);
+        while(1){
+            f = ans[f];
+            if(f == -1)
+                break;
+            else
+                temp.push_back(nums[f]);
+        }
+        return temp;
+    }
+};
