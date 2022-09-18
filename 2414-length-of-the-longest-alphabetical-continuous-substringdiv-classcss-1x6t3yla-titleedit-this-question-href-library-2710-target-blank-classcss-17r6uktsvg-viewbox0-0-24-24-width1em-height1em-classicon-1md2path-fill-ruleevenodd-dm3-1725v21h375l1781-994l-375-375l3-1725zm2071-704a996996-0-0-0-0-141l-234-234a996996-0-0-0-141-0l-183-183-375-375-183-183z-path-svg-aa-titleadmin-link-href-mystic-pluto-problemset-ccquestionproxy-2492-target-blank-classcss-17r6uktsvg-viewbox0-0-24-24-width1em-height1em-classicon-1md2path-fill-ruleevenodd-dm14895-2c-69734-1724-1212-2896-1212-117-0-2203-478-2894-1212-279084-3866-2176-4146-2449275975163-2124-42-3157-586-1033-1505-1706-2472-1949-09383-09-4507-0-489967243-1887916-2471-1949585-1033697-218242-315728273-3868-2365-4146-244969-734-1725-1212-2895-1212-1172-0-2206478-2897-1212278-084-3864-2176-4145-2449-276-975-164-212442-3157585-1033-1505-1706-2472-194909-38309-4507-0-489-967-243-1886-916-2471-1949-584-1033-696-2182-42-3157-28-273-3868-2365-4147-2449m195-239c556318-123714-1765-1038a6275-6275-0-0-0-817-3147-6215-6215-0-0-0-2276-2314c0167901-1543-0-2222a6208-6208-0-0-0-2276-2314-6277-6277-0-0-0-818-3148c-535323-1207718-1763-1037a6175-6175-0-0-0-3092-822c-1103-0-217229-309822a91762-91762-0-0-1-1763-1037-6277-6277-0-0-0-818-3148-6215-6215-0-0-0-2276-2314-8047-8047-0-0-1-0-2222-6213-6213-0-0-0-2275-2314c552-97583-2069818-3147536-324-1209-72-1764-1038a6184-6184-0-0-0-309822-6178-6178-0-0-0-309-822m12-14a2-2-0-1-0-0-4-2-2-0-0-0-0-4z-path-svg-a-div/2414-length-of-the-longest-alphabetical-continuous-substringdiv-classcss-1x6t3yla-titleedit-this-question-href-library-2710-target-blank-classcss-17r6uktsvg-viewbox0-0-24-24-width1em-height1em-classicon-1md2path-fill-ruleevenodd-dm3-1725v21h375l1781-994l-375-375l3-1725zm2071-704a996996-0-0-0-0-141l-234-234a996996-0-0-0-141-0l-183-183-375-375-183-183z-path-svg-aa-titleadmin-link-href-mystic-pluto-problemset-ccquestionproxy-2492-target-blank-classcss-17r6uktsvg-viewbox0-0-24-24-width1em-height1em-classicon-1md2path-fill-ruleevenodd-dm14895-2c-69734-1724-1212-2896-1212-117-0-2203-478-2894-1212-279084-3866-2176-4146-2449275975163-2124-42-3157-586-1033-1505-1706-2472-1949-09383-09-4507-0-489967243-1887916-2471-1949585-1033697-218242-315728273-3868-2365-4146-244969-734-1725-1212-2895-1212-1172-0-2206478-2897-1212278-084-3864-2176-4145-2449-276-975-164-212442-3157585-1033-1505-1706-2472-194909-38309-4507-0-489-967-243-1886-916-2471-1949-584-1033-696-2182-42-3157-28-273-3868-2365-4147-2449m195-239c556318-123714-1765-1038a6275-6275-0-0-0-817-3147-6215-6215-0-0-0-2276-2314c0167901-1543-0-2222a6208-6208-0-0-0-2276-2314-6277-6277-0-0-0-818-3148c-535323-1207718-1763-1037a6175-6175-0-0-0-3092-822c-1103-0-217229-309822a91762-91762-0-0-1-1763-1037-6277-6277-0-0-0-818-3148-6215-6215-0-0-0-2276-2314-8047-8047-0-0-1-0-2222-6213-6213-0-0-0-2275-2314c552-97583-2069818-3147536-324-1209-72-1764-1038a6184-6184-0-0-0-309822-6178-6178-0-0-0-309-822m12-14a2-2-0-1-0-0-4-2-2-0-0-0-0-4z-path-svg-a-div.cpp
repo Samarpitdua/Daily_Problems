72b3e16ad2933b9044ff8775d7b33ece9cc1d312
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-09-18 11:01:25 +0530

    Time: 154 ms (28.57%), Space: 15.3 MB (14.29%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substringdiv-classcss-1x6t3yla-titleedit-this-question-href-library-2710-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-2492-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div/2414-length-of-the-longest-alphabetical-continuous-substringdiv-classcss-1x6t3yla-titleedit-this-question-href-library-2710-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-2492-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substringdiv-classcss-1x6t3yla-titleedit-this-question-href-library-2710-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-2492-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div/2414-length-of-the-longest-alphabetical-continuous-substringdiv-classcss-1x6t3yla-titleedit-this-question-href-library-2710-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm3-1725v21h375l1781-994l-375-375l3-1725zm2071-704a996996-0-0-0-0-141l-234-234a996996-0-0-0-141-0l-183-183-375-375-183-183z-path-svg-aa-titleadmin-link-href-mystic-pluto-problemset-ccquestionproxy-2492-target-blank-classcss-17r6uktsvg-viewbox0-0-24-24-width1em-height1em-classicon-1md2path-fill-ruleevenodd-dm14895-2c-69734-1724-1212-2896-1212-117-0-2203-478-2894-1212-279084-3866-2176-4146-2449275975163-2124-42-3157-586-1033-1505-1706-2472-1949-09383-09-4507-0-489967243-1887916-2471-1949585-1033697-218242-315728273-3868-2365-4146-244969-734-1725-1212-2895-1212-1172-0-2206478-2897-1212278-084-3864-2176-4145-2449-276-975-164-212442-3157585-1033-1505-1706-2472-194909-38309-4507-0-489-967-243-1886-916-2471-1949-584-1033-696-2182-42-3157-28-273-3868-2365-4147-2449m195-239c556318-123714-1765-1038a6275-6275-0-0-0-817-3147-6215-6215-0-0-0-2276-2314c0167901-1543-0-2222a6208-6208-0-0-0-2276-2314-6277-6277-0-0-0-818-3148c-535323-1207718-1763-1037a6175-6175-0-0-0-3092-822c-1103-0-217229-309822a91762-91762-0-0-1-1763-1037-6277-6277-0-0-0-818-3148-6215-6215-0-0-0-2276-2314-8047-8047-0-0-1-0-2222-6213-6213-0-0-0-2275-2314c552-97583-2069818-3147536-324-1209-72-1764-1038a6184-6184-0-0-0-309822-6178-6178-0-0-0-309-822m12-14a2-2-0-1-0-0-4-2-2-0-0-0-0-4z-path-svg-a-div.cpp
@@ -3,11 +3,9 @@ public:
     int longestContinuousSubstring(string s) {
         vector<int> v;
         int ans = 1 , ct = 1;
-        for(auto x : s)
-            v.push_back(x - 'a');
-        for(int i = 0 ; i < v.size() - 1 ;i++)
+        for(int i = 0 ; i < s.size() - 1 ;i++)
         {
-            if(v[i] + 1 == v[i + 1])
+            if(s[i] + 1 == s[i + 1])
             {
                 ct++;
             }
