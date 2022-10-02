359abc0029af9e5a2f2802297730866bc42c84b4
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-10-02 17:56:45 +0530

    Time: 84 ms (7.06%), Space: 13.3 MB (12.13%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,54 @@
 class Solution {
 public:
-    int solve(string& s , string& t , int i , int j , vector<vector<int>>&  dp)
-    {
-        if(i < 0 or j < 0)
+    int solve(string& s , string& t , int m , int n , vector<vector<int>>& dp){
+        if(m < 0 or n < 0)
             return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(s[i] == t[j])
-        {
-            return dp[i][j] = 1 + solve(s , t , i - 1 , j - 1 , dp);
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        if(s[m] == t[n]){
+            return dp[m][n] = 1 + solve(s , t ,m - 1 , n - 1 , dp);
         }
-        return dp[i][j] = max(solve(s , t , i - 1 , j , dp) ,  solve(s , t , i , j - 1 , dp));
+        else
+            return dp[m][n] = max(solve(s , t , m - 1 , n , dp) , solve(s , t ,m , n - 1 , dp));
     }
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size() , m = text2.size();
-        vector<vector<int>> dp(n , vector<int>(m , -1));
-        return solve(text1 , text2 , n - 1 , m - 1 , dp);
+        int m = text1.size() , n = text2.size();
+        vector<vector<int>> dp(m , vector<int>(n , -1));
+        return solve(text1 , text2 , m - 1, n - 1, dp);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int n = text1.size() , m = text2.size();
+        // vector<vector<int>> dp(n + 1 , vector<int> (m + 1 , 0));
+        // for(int i = 1 ; i <= n ;i++)
+        // {
+        //     for(int j = 1 ; j <= m ;j++)
+        //     {
+        //         if(text1[i - 1] == text2[j - 1])
+        //         {
+        //             dp[i][j] = 1 + dp[i - 1][j - 1];
+        //         }
+        //         else
+        //             dp[i][j] = max(dp[i - 1][j] , dp[i][j - 1]);
+        //     }
+        // }
+        // return dp[n][m];
     }
 };
