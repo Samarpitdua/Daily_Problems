5b4102ca4ebf271eca4d3fd56210da23538817bc
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-10-04 23:29:23 +0530

    Time: 6 ms (89.77%), Space: 7.4 MB (32.79%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    # define MOD 1000000
+    int rabinKarp(string s , string t)
+    {
+        int m = t.size();
+        int power = 1;
+        for(int i = 0 ; i < m ;i++)
+        {
+            power *= 31;
+            power %= MOD;
+        }
+        int patt_hash = 0;
+        for(int i = 0 ; i < m ; i++)
+        {
+            patt_hash = (patt_hash * 31 + t[i]) % MOD;
+        }
+        int st_hash = 0;
+        for(int i = 0 ; i < s.size() ;i++){
+            st_hash = (st_hash * 31 + s[i]) % MOD;
+            if(i < m - 1)
+                continue;
+            if(i >= m)
+            {
+                st_hash = (st_hash - s[i - m] * power) % MOD;
+            }
+            if(st_hash < 0)
+                st_hash += MOD;
+            
+            if(st_hash == patt_hash)
+            {
+                if(s.substr(i - m + 1 , m) == t)
+                    return 1;
+                
+            }
+        }
+        return -1;
+    }
+    int repeatedStringMatch(string s, string t) {
+        if(s == t)
+            return 1;
+        string temp = "";
+        int ct = 0;
+        while(temp.size() < t.size())
+            ct++ , temp += s;
+        if(rabinKarp(temp , t) != -1)
+            return ct;
+        if(rabinKarp(temp + s, t) != -1)
+            return ct + 1;
+        return -1;
+        
+        
+    }
+};
