fe0cd4fbab272993a8c0a199675fbe83ff5ff49c
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-10-05 00:52:23 +0530

    Time: 5 ms (75.00%), Space: 8.7 MB (8.94%) - LeetHub

---
+++ 249-group-shifted-strings/249-group-shifted-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        map<vector<int>,vector<string>> mp;
+        for(string &st: strings)
+        {
+            vector<int> temp;
+            for(int i=1; i<st.size(); i++) 
+                temp.push_back((26+st[i]-st[i-1])%26);
+            mp[temp].push_back(st);
+        }
+        vector<vector<string>> ans;
+        for(auto &t:mp) ans.push_back(t.second);
+        return ans;
+    }
+};
