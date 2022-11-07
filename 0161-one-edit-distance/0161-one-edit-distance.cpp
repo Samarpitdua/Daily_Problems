e578f8793194f6b0a8516b38e6695efe32dcbce8
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-11-07 22:04:32 +0530

    Time: 2081 ms (5.12%), Space: 427.1 MB (5.46%) - LeetHub

---
+++ 0161-one-edit-distance/0161-one-edit-distance.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(string& word1 , string& word2 , int i , int j , vector<vector<int>>& dp)
+    {
+        if(i < 0 and j < 0)
+        {
+            return 0;
+        }
+        if(i < 0)
+        {
+            return (j + 1);
+        }
+        if(j < 0)
+        {
+            return (i + 1);
+        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        if(word1[i] == word2[j])
+        {
+            return dp[i][j] = solve(word1 , word2 , i - 1 , j - 1 , dp);
+        }
+        else
+            return dp[i][j] = 1 + min({solve(word1 , word2 , i - 1 , j, dp) , solve(word1 , word2 , i , j - 1, dp) , solve(word1 , word2 , i - 1 , j - 1, dp)});
+    }
+    bool isOneEditDistance(string word1, string word2) {
+        int m = word1.size() , n = word2.size();
+        vector<vector<int>> dp(m + 1 , vector<int> (n , -1));
+        if(solve(word1 , word2 , m - 1 , n - 1 , dp) == 1)
+            return 1;
+        return 0;
+    }
+};
