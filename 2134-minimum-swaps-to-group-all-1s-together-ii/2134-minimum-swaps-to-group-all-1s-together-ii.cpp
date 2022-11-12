e7bbacb4b7f983f4f2fa3584f7466c838ee1455f
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-11-12 18:34:30 +0530

    Time: 272 ms (20.56%), Space: 88.6 MB (13.24%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& data) {
+        int n = data.size(), ct = 0;
+        for(int i = 0 ;  i < n ;i++){
+            if(data[i])
+                ct++;
+            data.push_back(data[i]);
+        }
+        n *= 2;
+        if(ct == 1 or !ct) return 0;
+        
+        int ans = INT_MAX, swap = 0, init = 0;
+        
+        for(int i = 0, j = 0 ; i < n ;i++){
+            if(data[i] == 1)
+                init++;
+            else
+                swap++;
+            if(init + swap == ct)
+            {
+                ans = min(ans , swap);
+                if(data[j] == 1)
+                    init--;
+                else
+                    swap--;
+                j++;
+            }
+        }
+        return ans;
+    }
+};
