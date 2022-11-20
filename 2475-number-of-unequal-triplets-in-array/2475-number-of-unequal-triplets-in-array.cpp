3f7bcade7214bf29ce462e26e02a16074580289e
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-11-20 10:47:01 +0530

    Time: 4 ms (81.82%), Space: 8.5 MB (36.36%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     int unequalTriplets(vector<int>& nums) {
         int n = nums.size() , ct = 0;
-        for(int i = 0 ;  i < n - 2;i++){
-            for(int j = i + 1 ; j  < n - 1;j++){
-                for(int k = j + 1 ; k < n ;k++){
-                    if(nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k])
-                        ct++;
-                }
-            }
-        }
-        return ct;
+        // for(int i = 0 ;  i < n - 2;i++){
+        //     for(int j = i + 1 ; j  < n - 1;j++){
+        //         for(int k = j + 1 ; k < n ;k++){
+        //             if(nums[i] != nums[j] and nums[i] != nums[k] and nums[j] != nums[k])
+        //                 ct++;
+        //         }
+        //     }
+        // }
+        // return ct;
+        
+    unordered_map<int, int> mp;
+    for (auto x : nums)
+        mp[x]++;
+    int res = 0, left = 0, right = nums.size();
+    for (auto x : mp) {
+        right -= x.second;
+        res += left * x.second * right;
+        left += x.second;
+    }
+    return res;
     }
 };
