e252a1aceac8e23662e05041796767a644a8ac96
Samarpitdua <65570761+Samarpitdua@users.noreply.github.com>
2022-12-05 23:14:58 +0530

    Time: 133 ms (73.25%), Space: 17 MB (22.11%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-int solve(int i , int j1 , int j2 , vector<vector<int>>& grid , vector<vector<vector<int>>>& dp , int n)
+    int solve(int i , int j , int k , vector<vector<vector<int>>>& dp , vector<vector<int>>& grid, int m)
     {
-        if(j1 >= n or j1 < 0 or j2 >= n or j2 < 0)
-            return -1e8;
-        
-        if(i == grid.size() - 1)
+        if(k == m)
         {
-            if(j1 == j2)
-                return grid[i][j1];
-            else
-                return grid[i][j1] + grid[i][j2];
+            return 0;
         }
+        if(i < 0 or i == grid[0].size() or j < 0 or j == grid[0].size())
+            return 0;
         
-        if(dp[i][j1][j2] != -1)
-            return dp[i][j1][j2];
+        if(dp[i][j][k] != -1)
+            return dp[i][j][k];
         
-        int maxi = -1e8;
-        for(int j11 = -1 ; j11 <= 1 ;j11++){
-            for(int j22 = -1 ;j22 <= 1 ;j22++){
-                int val = 0;
-                if(j1 == j2)
-                    val += grid[i][j1];
-                else
-                    val += grid[i][j1] + grid[i][j2];
-                maxi = max(maxi , val + solve(i + 1 , j1 + j11 , j2 + j22, grid , dp, n));
-                    
-            }
+        if(i == j)
+        {
+            return dp[i][j][k] = grid[k][i] + max({solve(i + 1 , j + 1 , k + 1, dp , grid , m) , solve(i - 1 , j + 1 , k + 1, dp , grid , m) , solve(i + 1 , j - 1 , k + 1, dp , grid , m) , solve(i - 1 , j - 1 , k + 1, dp , grid , m) , solve(i , j + 1 , k + 1, dp , grid , m) , solve(i , j - 1 , k + 1, dp , grid , m)  , solve(i + 1 , j , k + 1, dp , grid , m) , solve(i - 1 , j , k + 1, dp , grid , m) , solve(i , j, k + 1, dp , grid , m)});
         }
-        return dp[i][j1][j2] = maxi;
+        else 
+            return dp[i][j][k] = grid[k][j] + grid[k][i] + max({solve(i + 1 , j + 1 , k + 1, dp , grid , m) , solve(i - 1 , j + 1 , k + 1, dp , grid , m) , solve(i + 1 , j - 1 , k + 1, dp , grid , m) , solve(i - 1 , j - 1 , k + 1, dp , grid , m) , solve(i , j + 1 , k + 1, dp , grid , m) , solve(i , j - 1 , k + 1, dp , grid , m)  , solve(i + 1 , j , k + 1, dp , grid , m) , solve(i - 1 , j , k + 1, dp , grid , m) , solve(i , j, k + 1, dp , grid , m)});
     }
+    
+    
     int cherryPickup(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<vector<int>>> dp(rows , vector<vector<int>>(cols , vector<int>(cols , -1)));
-        return solve(0 , 0 , cols - 1 , grid , dp , cols);
-        
+        int n = grid.size() , m = grid[0].size();
+        vector<vector<vector<int>>> dp(m , vector<vector<int>>(m , vector<int>(n , -1)));
+        return solve(0 , m - 1 , 0 , dp , grid, n);
     }
 };
